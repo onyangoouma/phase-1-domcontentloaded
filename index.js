6a3bd0858da3b1f6d356ffe5e5ce6041df0885dc
onyangoouma <jacksonouma04@gmail.com>
2022-06-20 09:19:40 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener( "DOMContentLoaded", function () {
+    changeText()
+  } );
+ 
+  function changeText() {
+    const pText = document.getElementById( "text" )
+      pText.innerHTML = "This is really cool!";
+  }
